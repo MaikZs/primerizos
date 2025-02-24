eaa7561e58ce241916f529bf756c25c944d68f8c
MaikZs <araujo.ncc@gmail.com>
2025-02-23 20:43:13 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,6 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/primerizos' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/primerizos/' : '',
 };
 
 export default nextConfig;
