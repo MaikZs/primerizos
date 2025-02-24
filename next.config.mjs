60ad33de1ff95a6c0b50abd9e6926764bfa7eebe
MaikZs <araujo.ncc@gmail.com>
2025-02-23 20:22:09 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',  // Necesario para GitHub Pages
     images: {
-      unoptimized: true,
+        unoptimized: true,
     },
-  };
-    export default nextConfig;
+    basePath: process.env.NODE_ENV === 'production' ? '/primerizos' : '', // Ajusta 'primerizos' al nombre exacto de tu repositorio
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/primerizos/' : '', // Ajusta 'primerizos' al nombre exacto de tu repositorio
+};
+
+export default nextConfig;
