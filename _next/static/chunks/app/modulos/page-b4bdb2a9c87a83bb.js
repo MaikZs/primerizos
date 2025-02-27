87f3837f80f6c4e863ab16f13ab28f611341a68c
MaikZs <MaikZs@users.noreply.github.com>
2025-02-27 03:35:34 +0000

    deploy: 8e4e545d52b8341ab8dbcdff50b44a35c9cea954

---
+++ _next/static/chunks/app/modulos/page-b4bdb2a9c87a83bb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[42,61,570,974],{7020:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,7970,23)),Promise.resolve().then(n.bind(n,3449))}},e=>{var s=s=>e(e.s=s);e.O(0,[324,992,895,253,449,441,517,358],()=>s(7020)),_N_E=e.O()}]);
